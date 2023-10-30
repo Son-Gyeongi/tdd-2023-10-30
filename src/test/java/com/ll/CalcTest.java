513344145e8f299b1d2aff06de62c08377fec249
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-30 11:10:36 +0900

    10 -10 =0

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -5,6 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * TDD
+ * 테스트 주도 개발
+ * 테스트 코드 -> 구현 -> 배포
+ * <p>
+ * red-green-refactoring 반복
+ * RED : 실패하는 테스트케이스를 만든다.
+ *
+ * GREEN : 최대한 꼼수를 써서 테스트케이스를 통과하게 만든다.
+ *
+ * REFACTORING : 중복을 제거한다.
+ */
 public class CalcTest {
 
     @Test
@@ -19,5 +31,13 @@ public class CalcTest {
         int rs = Calc.run("10 + 10");
         // 요구사항 작성
         assertThat(rs).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 + 10 => 0")
+    void t3() {
+        int rs = Calc.run("10 - 10");
+
+        assertThat(rs).isEqualTo(0);
     }
 }
