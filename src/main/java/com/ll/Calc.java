e63a3492304f71301b2d00e0ee98d13ede8c7e8d
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-30 11:38:28 +0900

    Clac class 위치 변경

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,7 +3,7 @@ package com.ll;
 public class Calc {
 
     public static int run(String exp) {
-        // 값이 없는 경우, 비어있는 지 체크
+        // 값이 없는 경우, 비어있는지 체크
         if (exp.isBlank()) return 0;
 
         if (exp.contains("-")) {
